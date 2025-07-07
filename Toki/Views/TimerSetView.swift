6af7ab0cdcb7fa52c7dd4fd0aa56fef6cc16ae5f
xaxaa <62248038+deerg01@users.noreply.github.com>
2025-07-08 01:38:24 +0900

    add fundamental notice functions. minor fix on message part

---
+++ Toki/Views/TimerSetView.swift
@@ -0,0 +1,21 @@
+//
+//  TimerSetView.swift
+//  Toki
+//
+//  Created by POS on 7/7/25.
+//
+
+import Foundation
+import SwiftData
+import SwiftUI
+
+struct TimerSetView: View {
+    var body: some View {
+        Text("Hello World!")
+        Spacer()
+    }
+}
+
+#Preview {
+    TimerSetView()
+}
