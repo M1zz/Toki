ac28e2ba0db94030a4ef680b68f235c126631f3c
xaxaa <62248038+deerg01@users.noreply.github.com>
2025-07-20 18:51:41 +0900

    #9 navigation 연결 완료. 누락된 파일 push
    TimerView에서 여러 설정창으로 넘어가는 플로우 완성했습니다
    이전 커밋에서 'ControlButton' 에 대한 파일을 하나도 안올려버리기.. 그래서 지금 올립니다

---
+++ Toki/Views/TemplateView.swift
@@ -0,0 +1,15 @@
+//
+//  TemplateView.swift
+//  Toki
+//
+//  Created by POS on 7/20/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct TemplateView: View {
+    var body: some View {
+        Text("유저가 저장한 세 개의 알림 설정 데이터를 표시합니당")
+    }
+}
