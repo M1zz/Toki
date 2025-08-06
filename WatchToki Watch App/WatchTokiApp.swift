71433c8fd8b304caa65d6b6196895b61341ad223
Hye <gahyejeon@gmail.com>
2025-08-07 00:37:26 +0900

    design: 타이머 시간 설정 화면 UI 배치

---
+++ WatchToki Watch App/WatchTokiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTokiApp.swift
+//  WatchToki Watch App
+//
+//  Created by 내꺼다 on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchToki_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
